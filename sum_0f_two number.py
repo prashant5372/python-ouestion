5ffa19a2faa66b58402492f0836fd8013a4a9d9c
prashant5372 <prashantpranav51@gmail.com>
2024-09-26 13:27:26 +0530

    sum of two number

---
+++ sum_0f_two number.py
@@ -0,0 +1,38 @@
+# sum of two number
+"""
+a=4
+b=2
+sum=a+b
+print("sum of two number")
+print(sum)
+print("sum of two number",sum)
+print("product",a*b)
+print("divide",a/b)
+print("remainder",a%b)
+#power operator
+print(a**b)
+print(a==b)
+print(a!=b)
+#assigment opertor
+num=10
+num=num+10
+print("num :",num)
+#logical operator
+print(not (a>b))
+print(not(b>a))
+#type casting
+a=int("3")
+b=2.3
+print(a+b)
+print(type(a))
+a=2.5
+a=str(a)
+print(type(a))
+#input in python
+name=input("enter you name baby =")
+print("welcome",name)
+#value=input("enter the value")
+value=int(input("enter the value"))
+print(type(value),value)
+"""
+#taking input of various thing
