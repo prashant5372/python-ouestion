f96700e38c84e5b7701d39c8419ab253b84a8651
prashant5372 <prashantpranav51@gmail.com>
2024-09-26 13:21:11 +0530

    multiple of seven

---
+++ multiple-of 7.py
@@ -0,0 +1,8 @@
+#WAP to check if a number is multiple of 7
+num1=int(input("enter the value"))
+
+if(num1%7==0):
+    print("given number is multiple of seven")
+else:
+    print("given number is not multiple of seven")    
+
