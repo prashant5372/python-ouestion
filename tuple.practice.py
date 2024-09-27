fa6b6ea48042d9e30f37645b1402d2c42bd4434c
prashant5372 <prashantpranav51@gmail.com>
2024-09-27 12:19:44 +0530

    practice

---
+++ tuple.practice.py
@@ -0,0 +1,16 @@
+#WAP to ask the user to enter name of their 3 fav movie &store them in list
+"""user1=input("enter you movie name")
+user2=input("enter you movie name")
+user3=input("enter you movie name")
+list=[user1,user2,user3]
+print(list)
+print(type(user1))
+"""
+# another way
+
+movies=[]
+movies.append(input("enter 1st movie:"))
+movies.append(input("enter2nd movie:"))
+movies.append(input("enter 3rd movie:"))
+print(movies)
+
